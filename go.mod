67b2bf54e87de39820f9de2e711e77d2c954fe6d
arews-cn <arewss@dingtalk.com>
2023-05-05 01:46:54 +0800

    域名对比

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module bountytracker
+module github.com/arews-cn/bountytracker
 
 go 1.20
+
+require (
+	github.com/edsrzf/mmap-go v1.1.0
+	golang.org/x/sys v0.8.0
+)
--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
 	"regexp"
 	"sort"
+	"strings"
+
+	"github.com/edsrzf/mmap-go"
+
+	"golang.org/x/sys/unix"
 )
 
 var bugcrowdurl = "https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/bugcrowd_data.json"
 var hackeroneurl = "https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/hackerone_data.json"
+
+var source_path = filepath.Join(user_home_dir(), ".config/bountytr/")
 
 type HackeroneScope struct {
 	AssetIdentifier           string `json:"asset_identifier"`
@@ -87,7 +100,7 @@ func BountyTarget(url string) []byte {
 	return body
 
 }
-func bugcrowd() {
+func bugcrowd(source_targets map[string]bool) (new_targets []string) {
 
 	var body = BountyTarget(bugcrowdurl)
 
@@ -111,7 +124,10 @@ func bugcrowd() {
 			// 只打印 Web 目标
 			if in(scope.Type, []string{"api", "website"}) {
 				for _, domain := range domain_match(scope.Target) {
-					fmt.Println(domain)
+					if !source_targets[domain] {
+						fmt.Println(domain)
+						new_targets = append(new_targets, domain)
+					}
 				}
 			}
 
@@ -119,9 +135,11 @@ func bugcrowd() {
 
 	}
 
+	return
+
 }
 
-func hackerone() {
+func hackerone(source_targets map[string]bool) (new_targets []string) {
 
 	var body = BountyTarget(hackeroneurl)
 
@@ -146,21 +164,61 @@ func hackerone() {
 			if in(scope.AssetType, []string{"URL", "WILDCARD"}) {
 
 				for _, domain := range domain_match(scope.AssetIdentifier) {
-					fmt.Println(domain)
+					if !source_targets[domain] {
+						fmt.Println(domain)
+						new_targets = append(new_targets, domain)
+					}
 				}
-
 			}
 		}
 	}
+
+	return
 }
 
 func main() {
 
-	fmt.Println("New Bounty Target ....")
-	hackerone()
-	bugcrowd()
+	var banner = `
+
+         __                      __        __      
+        / /_  ____  __  ______  / /___  __/ /______
+       / __ \/ __ \/ / / / __ \/ __/ / / / __/ ___/
+      / /_/ / /_/ / /_/ / / / / /_/ /_/ / /_/ /    
+     /_.___/\____/\__,_/_/ /_/\__/\__, /\__/_/     
+                                 /____/       v1.0       
+
+   
+	Keep track of bounty targets
+    `
+	fmt.Println(string(banner))
+
+	var cycle_time int64
+
+	// now := time.Now().Format("2006-01-02 15:04:05")
+
+	flag.Int64Var(&cycle_time, "t", 30, "监控周期(分钟)")
+
+	// 解析命令行参数写入注册的flag里
+	flag.Parse()
+
+	fmt.Println("[*] Starting tracker", "... ")
+
+	os.MkdirAll(source_path, os.ModePerm)
+	source_targets := read_file_to_map(filepath.Join(source_path, "domain.txt"))
+
+	new_hackerone_targets := hackerone(source_targets)
+	new_bugcrowd_targets := bugcrowd(source_targets)
+
+	// 文件末尾追加新目标
+	save_targets_to_file(filepath.Join(source_path, "domain.txt"), append(new_hackerone_targets, new_bugcrowd_targets...))
 
 }
+
+// // 目标对比
+// func compared_target(latest_targets []string) {
+// 	// 获取新资产目标，对比出新增资产
+
+// }
 
 func in(target string, str_array []string) bool {
 	sort.Strings(str_array)
@@ -176,4 +234,65 @@ func domain_match(url string) []string {
 	domain_rege := regexp.MustCompile(`[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+`)
 
 	return domain_rege.FindAllString(url, -1)
+}
+
+func read_file_to_map(filename string) map[string]bool {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	size := info.Size()
+
+	if size == 0 {
+		if _, err := file.WriteString("\n"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	hash := make(map[string]bool)
+	reader := bufio.NewReader(file)
+	mm, err := mmap.Map(file, unix.PROT_READ, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mm.Unmap()
+
+	for i := 0; int64(i) < size; i++ {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		hash[strings.Replace(line, "\n", "", -1)] = true
+	}
+	return hash
+}
+
+func save_targets_to_file(filename string, targets []string) {
+
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, target := range targets {
+		writer.WriteString(target + "\n")
+	}
+	writer.Flush()
+	file.Sync()
+
+}
+func user_home_dir() string {
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println("Could not get user home directory:", err)
+	}
+	return usr.HomeDir
 }
