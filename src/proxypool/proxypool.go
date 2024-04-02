b9813566c44f4c442ab7909e7080e6e6c82250ba
baiqll <arewss@dingtalk.com>
2024-04-03 01:42:52 +0800

    测试

--- src/proxypool/proxypool.go
+++ src/proxypool/proxypool.go
@@ -35,6 +35,7 @@ func NewProxyPool() *ProxyPool {
 
 	return &ProxyPool{
 		Source: "https://list.proxylistplus.com/Fresh-HTTP-Proxy-List-$",
+		// Source: "https://list.proxylistplus.com/Socks-List-$",
 	}
 }
 
@@ -62,7 +63,7 @@ func (h ProxyPool) GetProxyList(index int) (proxies []string) {
 
 			if len(td_content) > 0 {
 
-				proxies = append(proxies, fmt.Sprintf("http://%s:%s", td_content[1], td_content[2]))
+				proxies = append(proxies, fmt.Sprintf("%s://%s:%s", td_content[3], td_content[1], td_content[2]))
 			}
 
 		})
@@ -74,13 +75,55 @@ func (h ProxyPool) GetProxyList(index int) (proxies []string) {
 
 func (h ProxyPool) InitPool() (pool Pool) {
 
-	var proxies []string
-
-	for i := 1; i <= 6; i++ {
-
-		proxies = append(proxies, h.GetProxyList(i)...)
-
+	var proxies = []string{
+		"http://115.205.239.179:7890",
+		"http://77.46.138.38:8080",
+		"https://111.224.10.230:8089",
+		"http://202.110.67.141:9091",
+		"http://217.52.247.89:1981",
+		"http://111.59.10.36:9002",
+		"http://8.219.5.240:8081",
+		"http://114.233.216.149:7788",
+		"http://201.182.251.142:999",
+		"http://47.109.53.253:45554",
+		"http://117.1.107.89:4014",
+		"http://167.71.5.83:8080",
+		"https://180.121.129.43:8899",
+		"http://102.218.160.132:80",
+		"http://41.77.188.131:80",
+		"http://124.70.55.29:87",
+		"http://117.70.48.139:8089",
+		"http://119.13.111.169:20201",
+		"https://115.74.163.47:4007",
+		"https://43.251.117.67:45787",
+		"https://117.57.92.235:8089",
+		"http://80.74.77.48:80",
+		"http://134.35.221.24:8080",
+		"http://93.1.195.28:8080",
+		"https://43.251.119.55:45787",
+		"https://111.224.11.176:8089",
+		"http://171.249.92.16:4011",
+		"http://221.6.139.190:9002",
+		"http://156.192.170.244:8080",
+		"http://124.70.205.56:50001",
+		"http://8.208.89.32:8081",
+		"http://120.82.174.128:9091",
+		"http://64.225.8.132:9981",
+		"http://183.164.242.55:8089",
+		"http://111.224.213.219:8089",
+		"http://47.254.47.61:443",
+		"http://171.233.132.239:4014",
+		"http://47.92.239.69:9992",
+		"http://111.225.152.161:8089",
+		"http://47.113.219.226:9091",
+		"http://60.12.168.114:9002",
 	}
+
+	// for i := 1; i <= 6; i++ {
+
+	// 	proxies = append(proxies, h.GetProxyList(i)...)
+
+	// }
 
 	h.Pool = *NewPool(proxies)
 
